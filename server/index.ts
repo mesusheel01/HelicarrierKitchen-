6795e3fa2455d39386ca8c748f28ef18c9d59f13
susheel <susheelrai96@gmail.com>
2026-05-09 07:19:03 +0000

    Setup db with prisma!

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3002
+
+// req.body accepting middleware
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Hi server is live!"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+})
